0eb05e04afa2f2fd0fff5fa3c74b7c2e2b64b437
Fausto Carvalho Marques Silva <faustocarva@gmail.com>
2014-12-15 00:04:55 -0200

    new tests

--- src/meta-hephaestus/HplProducts/samples/dtmcComposed/generate_pDTMC_Example_03.sh
+++
@@ -1,2 +0,0 @@
-cd /home/fausto/Dropbox/gits/faustocarva/hephaestus-pl/src/meta-hephaestus/HplProducts/
-echo -e "/home/fausto/Dropbox/gits/faustocarva/hephaestus-pl/src/meta-hephaestus/HplProducts/samples/dtmcComposed/pDTMC_Example_03" |  /home/fausto/Dropbox/gits/faustocarva/hephaestus-pl/src/meta-hephaestus/HplProducts/Test
