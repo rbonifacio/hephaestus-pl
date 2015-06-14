18a270f862e1a4f136bae3998808ebb1b721f5ad
Alessandro <alessandro.leite@alessandro.cc>
2015-06-14 17:52:44 +0200

    Remove the unused json parser and include the LGPL license file

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
 none:
 
+clean:
+	@find . -name "*.o" -exec rm -rf {} \;
+	@find . -name "*.hi" -exec rm -rf {} \;
+	@cd src/meta-hephaestus; make clean
+	
+%-build:
+	@cd src/meta-hephaestus; make $*-build	
+
 push:
 	git commit -a
 	git push
--- src/meta-hephaestus/HplAssets/Cloud/Parsers/Yaml.hs
+++ src/meta-hephaestus/HplAssets/Cloud/Parsers/Yaml.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Cloud.Parsers.Yaml
@@ -17,10 +18,47 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Data.Yaml as Y
 
 import Data.Aeson
+import Control.Applicative
+import Control.Monad
 
 import BasicTypes
 import HplAssets.Cloud.Types
-import HplAssets.Cloud.Parsers.Json
+
+-- Instances to convert our type from JSON
+
+instance FromJSON CloudModel where
+  parseJSON (Object m) = 
+     CloudModel <$> m .: "clouds"      
+  parseJSON _ = mzero
+
+instance FromJSON Cloud where
+  parseJSON (Object m) =
+     Cloud <$> m .: "cloud-id"
+           <*> m .: "name"
+           <*> m .: "instances"  
+
+instance FromJSON Instance where
+  parseJSON (Object m) = 
+     Instance <$> m .: "instance-id"
+              <*> m .: "instance-name"           
+              <*> m .: "location"
+              <*> m .: "security-group"
+              <*> m .: "image-id"
+              <*> m .: "access-key"
+  parseJSON _ = mzero
+   
+--instance FromJSON Location where
+--  parseJSON (Object m) = 
+--     Location <$> m .: "region"
+--              <*> m .: "zone"
+--   parseJSON _ = mzero
+
+-- This is an elegant way to serialize Generic type as JSON. However, it can only be used
+-- iff the there isn't any customization on the attributes's name. It is important to highlight
+-- that the type to serialize must be Generic.
+instance FromJSON Location
+
+
 
 yamlFile :: FilePath
 yamlFile = "cloud-model.yaml"
